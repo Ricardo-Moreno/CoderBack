26d50ab740db0359e372b57edd9da093e9bbe9df
Ricardo Moreno <ricardo.yvl@gmail.com>
2023-10-31 18:51:10 -0300

    railway 6

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -3,7 +3,7 @@ import { logger } from '../utils/logger.js';
 
 export default async function configureMongo() {
   try {
-    const mongo = process.env.DB_USER;
+    const mongo = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
     await mongoose.connect(mongo, {
       useNewUrlParser: true,
       useUnifiedTopology: true
